551cbd0abdc911bcc7064b0d86db72a165c449b8
sivanWu <>
2018-07-07 17:29:25 +0800

    add network monitor

---
+++ NetworkMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkMonitor
+//
+//  Created by HFY on 2018/7/7.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkMonitor
+//
+//  Created by HFY on 2018/7/7.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
