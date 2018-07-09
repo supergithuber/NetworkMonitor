dafcd8da32a74894e3f418f506af87eaafb5c416
sivanWu <1984430988@qq.com>
2018-07-09 11:04:36 +0800

    add carthage

---
+++ NetworkCardMonitor/NetworkCardMonitor.h
@@ -0,0 +1,19 @@
+//
+//  NetworkCardMonitor.h
+//  NetworkCardMonitor
+//
+//  Created by HFY on 2018/7/9.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkCardMonitor.
+FOUNDATION_EXPORT double NetworkCardMonitorVersionNumber;
+
+//! Project version string for NetworkCardMonitor.
+FOUNDATION_EXPORT const unsigned char NetworkCardMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkCardMonitor/PublicHeader.h>
+
+#import <NetworkCardMonitor/WXNetworkMonitor.h>
